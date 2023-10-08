b2788a2a9cb65ad095050fc462f99ad18e46b05f
KhandekarPrateek <prateekkhandekar01@gmail.com>
2023-10-08 22:03:19 +0530

    background and modal in tenzies

---
+++ src/common/RulesModal/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+const RulesModal = (props) => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  return (
+    <div className="justify-content-center d-flex">
+      <Button color="primary" outline onClick={toggle}>
+        Rules
+      </Button>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>{props.title}</ModalHeader>
+        <ModalBody>{props.rules}</ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default RulesModal;
