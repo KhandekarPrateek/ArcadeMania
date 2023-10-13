2883507ba6a925d6bf42f89634d76b9d3dd58fb7
KhandekarPrateek <prateekkhandekar01@gmail.com>
2023-10-13 20:54:03 +0530

    the meme-page has been successfully completed

--- src/pages/MemesGame/components/FetchMeme/index.js
+++ src/pages/MemesGame/components/FetchMeme/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { useState, useEffect, useRef } from "react";
-import { Container, Row, Col, Button } from "reactstrap";
+import { Container, Row, Col, Button, Input } from "reactstrap";
 
 import html2canvas from "html2canvas";
 import Base64Downloader from "react-base64-downloader";
@@ -43,8 +43,6 @@ const FetchMeme = () => {
     });
     const image = canvas.toDataURL("meme.png");
     const imagestring = image.toString();
-    console.log(imagestring, "imagestring");
-    // console.log("canvas", canvas);
     setImageSS(imagestring);
   };
   useEffect(() => {
@@ -54,75 +52,74 @@ const FetchMeme = () => {
   return (
     <Container className="meme-page-container">
       <Row className=" w-100 h-100">
-        <Col>
-          <div className="form-memes ">
-            <Row>
-              {" "}
-              <input
-                type="text"
-                placeholder="upper field"
-                className="form--input"
-                name="topText"
-                onChange={handleChange}
-                value={memeInfo.topText}
+        <div className="p-5  h-100 w-50  align-items-center d-flex">
+          <Col className="justify-content-center d-flex">
+            <div className="w-75 h-100 border border-5   meme-form">
+              <Row>
+                <div className="p-5">
+                  <Input
+                    type="text"
+                    placeholder="upper field"
+                    className="form--input"
+                    name="topText"
+                    onChange={handleChange}
+                    value={memeInfo.topText}
+                  />
+                </div>{" "}
+              </Row>
+              <Row>
+                <div className="p-5">
+                  <Input
+                    type="text"
+                    placeholder="lower field"
+                    className="form--input"
+                    name="bottomText"
+                    onChange={handleChange}
+                    value={memeInfo.bottomText}
+                  />
+                </div>
+              </Row>
+              <Row>
+                <div className="justify-content-center d-flex p-5">
+                  <Button className="Download-button" onClick={handleClick}>
+                    {" "}
+                    Insert a new meme
+                  </Button>
+                </div>
+              </Row>
+              <Row>
+                <div className="justify-content-center d-flex p-5">
+                  <Base64Downloader
+                    onClick={printDocument}
+                    base64={imageSS}
+                    downloadName="meme"
+                    className="Download-button"
+                  >
+                    {/* <Button className="Download-button" outline> */}
+                    Download the meme
+                    {/* </Button> */}
+                  </Base64Downloader>
+                </div>
+              </Row>
+            </div>
+          </Col>
+        </div>
+        <div className="h-100 w-50 justify-content-center align-items-center d-flex  ">
+          <Col>
+            <div className="meme" ref={imageRef}>
+              <h2 className="meme--text meme-top">{memeInfo.topText}</h2>
+              <img
+                src={memeInfo.randomImage}
+                alt="memes to be pulled"
+                className="meme--image"
               />
-            </Row>
-            <Row>
-              <input
-                type="text"
-                placeholder="lower field"
-                className="form--input"
-                name="bottomText"
-                onChange={handleChange}
-                value={memeInfo.bottomText}
-              />
-            </Row>
-            <Row>
-              <div className="justify-content-center d-flex">
-                <Button color="info" outline onClick={handleClick}>
-                  {" "}
-                  Insert a new meme
-                </Button>
-              </div>
-            </Row>
-            <Row>
-              <div>
-                <Button>Download the meme</Button>
-              </div>
-              <div>
-                <Base64Downloader
-                  onClick={printDocument}
-                  base64={imageSS}
-                  downloadName="meme"
-                >
-                  Click to download
-                </Base64Downloader>
-              </div>
-            </Row>
-          </div>
-        </Col>
-        <Col className="justify-content-center align-item-center d-flex ">
-          <div className="meme" ref={imageRef}>
-            <h2 className="meme--text meme-top">{memeInfo.topText}</h2>
-            <img
-              src={memeInfo.randomImage}
-              alt="memes to be pulled"
-              className="meme--image"
-            />
 
-            <h2 className="meme--text meme-bottom">{memeInfo.bottomText}</h2>
-          </div>
-        </Col>
+              <h2 className="meme--text meme-bottom">{memeInfo.bottomText}</h2>
+            </div>
+          </Col>
+        </div>
       </Row>
     </Container>
   );
 };
 export default FetchMeme;
-//const table = document.getElementById("table-container");
-// html2canvas(imageRef.current).then(function (canvas) {
-//   const link = document.createElement("a");
-//   link.download = "meme.png";
-//   link.href = canvas.toDataURL("image/png");
-//   link.click();
-// });
-// console.log(imageRef.current, "ref value");
